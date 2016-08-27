7cde7b72286acd0b2367d7983736461001c119d4
lisers <lisers@qq.com>
2016-08-27 18:03:06 +0800

    test
    test

---
+++ brxx.js
@@ -0,0 +1,32 @@
+AutoForm.hooks({
+  'brxx-new-form': {
+    onSuccess: function (operation, result, template) {
+      IonModal.close();
+      IonKeyboard.close();
+	   console.log('brxx-new-form');
+      Router.go('brxx.show', {_id: result});
+    }
+  }
+});
+
+Template.br.onRendered(function () {
+  	Meteor.call('myaddress', Geolocation.latLng(), function(error, result) {
+   	  console.log(result);
+	  Session.set('myaddress', result);  	
+  	});
+
+});
+
+
+Template.br.helpers({
+  bdLocation: function () {
+    var latLng = Geolocation.latLng();
+
+    latLng=GPS.gcj_encrypt(latLng.lat,latLng.lng);
+
+    latLng=GPS.bd_encrypt(latLng.lat,latLng.lng);
+
+    return latLng.lng + "," + latLng.lat;
+  },
+  gaddress: function() { return Session.get('gaddress'); }
+});
